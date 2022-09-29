cbb7a74d2d2ae97f5c426099c3575bb52d42429f
30140 <3014085426@qq.com>
2022-09-29 21:07:23 +0800

    日常提交

--- src/BilibiliTest/Program.cs
+++ src/BilibiliTest/Program.cs
@@ -12,10 +12,10 @@ namespace BilibiliTest
         {
             BiliBiliArgs.TokenSESSDATA = token;
             Video video = new Video();
+            //把视频的基本信息API修改为以下，相对应的类也改了，明天改2022.9.29留
             var bv = await video.GetVideosContent("BV1Cg411u7Xr", Video.VideoIDType.BV);
-            UserVideo vid = new UserVideo();
-            var c =  await vid.LikeVideo(true,bv.Data.Aid);
-            Console.WriteLine(c.Data.TipText);
+            string url = $"https://app.bilibili.com/x/v2/view?aid={bv.Data.Aid}";
+            string a = await Test.Go(url);
             Console.ReadLine();
         }
     }
