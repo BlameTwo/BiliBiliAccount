0a4c55338515881e4d88dca2a6bffa73fc9ef787
一个渣渣 <65759143+BlameTwo@users.noreply.github.com>
2022-11-26 11:08:44 +0800

    增加热门搜索和搜索建议

--- src/BiliBiliAccount/Search/SearchSquare.cs
+++ src/BiliBiliAccount/Search/SearchSquare.cs
@@ -27,7 +27,11 @@ namespace BiliBiliAPI.Search
             return JsonConvert.ReadObject<SearchHotRankData>(await HttpClient.GetResults(url));
         }
 
-
+        /// <summary>
+        /// 搜索建议
+        /// </summary>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
         public async Task<SearchSuggestData> GetSearchSuggest(string keyword)
         {
             string url = $"https://s.search.bilibili.com/main/suggest?func=suggest&suggest_type=accurate&sub_type=tag&main_ver=v1&userid=108534711&bangumi_acc_num=1&special_acc_num=1&topic_acc_num=1&upuser_acc_num=3&tag_num=10&special_num=10&bangumi_num=10&upuser_num=3&term={keyword}&rnd=0.4120868569076672&buvid=3D0AE328BAD4804AE492C710221022160f0VHGRE8w0gPXftuGc3jQ&spmid=0";
